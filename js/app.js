afffd3e004eccc7dc7f3f670ade7cf413743bc28
Yuri Brunetto <yuribrunetto@hotmail.com.br>
2016-03-22 23:40:55 -0300

    add callback to gh-pages

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 SC.initialize({
     client_id: "b17b79b20e5c687a994a0d517dabe98a",
-    redirect_uri: "http://192.168.3.100:8080/callback.html"
+    redirect_uri: "http://yuribrunetto.github.io/soundcloud-uploader/callback.html"
 });
 
 $(function(){
