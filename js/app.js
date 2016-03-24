95c2680f0c018d3d09f84c18566d140120297969
Yuri Brunetto <yuribrunetto@hotmail.com.br>
2016-03-23 23:49:47 -0300

    beta version

--- js/app.js
+++ js/app.js
@@ -3,11 +3,71 @@ SC.initialize({
     redirect_uri: "http://yuribrunetto.github.io/soundcloud-uploader/callback.html"
 });
 
-$(function(){
+function updateTimer(ms) {
+    ms = ms / 1000;
+    $("#timer").html(Math.floor(ms));
+}
 
-    SC.connect().then(function(){
-        return SC.get("/me");
-    }).then(function(me){
-        alert("Hello, " + me.username + "!");
+$(function(){
+    $("#stopRecording, #play, .form, .timer").hide();
+
+    $("#startRecording").click(function(e){
+        e.preventDefault();
+        SC.record({
+            progress: function(ms){
+                updateTimer(ms);
+            }
+        });
+
+        $("#startRecording").hide();
+        $("#stopRecording, .timer").show();
+    });
+
+    $("#stopRecording").click(function(e){
+        e.preventDefault();
+
+        SC.recordStop();
+
+        $("#play, .form").show();
+        $("#stopRecording").hide();
+    });
+
+    $("#play").click(function(e){
+        e.preventDefault();
+
+        SC.recordPlay();
+    });
+
+    $('#upload').click(function(e) {
+        e.preventDefault();
+
+        var name    = $("#name").val(),
+            privacy = $("input[name=privacy]:checked").val();
+
+        console.log(privacy);
+
+        if (name == "" || privacy == "undefined") {
+            $("#name").css("border-color", "#e74c3c");
+            $("input[type=radio]").parent().parent().find("div.input").css("border-color", "#e74c3c");
+        } else {
+            $("#name").css("border-color", "#f9f9f9");
+            $("input[type=radio]").parent().parent().find("div.input").css("border-color", "#f9f9f9");
+            SC.connect({
+                connected: function(){
+                    $(".status").html("Uploading...");
+                    $(".form").slideUp();
+                    SC.recordUpload({
+                        track: {
+                            title: name,
+                            sharing: privacy
+                        }
+                    }, function(track) {
+                        $(".status").html(
+                            "Uploaded! <a href='" + track.permalink_url + "' target='_blank'>" + track.title + "</a>"
+                        );
+                    });
+                }
+            });
+        }
     });
 });
